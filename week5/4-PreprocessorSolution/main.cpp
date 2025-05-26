efe1c037b4e74558ab408c0bb0ba06b53ce4674a
JPBG-USP <joao_garciajp@usp.br>
2025-05-26 15:08:48 -0300

    Adding week5 exercises

---
+++ week5/4-PreprocessorSolution/main.cpp
@@ -0,0 +1,30 @@
+#define INF 100000000
+#define toStr(x) #x
+#define FUNCTION(name, op) void name(int &a, int b) { if (b op a) a = b; }
+#define foreach(v, i) for (int i = 0; i < (int)v.size(); ++i)
+#define io(v) cin >> v // Esse aqui sofri muuuito, mas deu bom
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+FUNCTION(minimum, <)
+FUNCTION(maximum, >)
+
+int main(){
+	int n; cin >> n;
+	vector<int> v(n);
+	foreach(v, i) {
+		io(v)[i];
+	}
+	int mn = INF;
+	int mx = -INF;
+	foreach(v, i) {
+		minimum(mn, v[i]);
+		maximum(mx, v[i]);
+	}
+	int ans = mx - mn;
+	cout << toStr(Result =) <<' '<< ans;
+	return 0;
+
+}
